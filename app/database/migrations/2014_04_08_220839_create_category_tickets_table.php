bcb201200bd8c1d792e006f8231a0dda099df2b9
fideloper <fideloper@gmail.com>
2014-04-08 18:22:32 -0400

    more migrations

--- app/database/migrations/2014_04_08_220839_create_category_tickets_table.php
+++ app/database/migrations/2014_04_08_220839_create_category_tickets_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagesTable extends Migration {
+class CreateCategoryTicketsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,12 +12,10 @@ class CreateMessagesTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('messages', function(Blueprint $table)
+		Schema::create('category_tickets', function(Blueprint $table)
 		{
-			$table->increments('id');
             $table->integer('ticket_id');
-            $table->text('note');
-			$table->timestamps();
+            $table->integer('category_id');
 		});
 	}
 
@@ -28,7 +26,7 @@ class CreateMessagesTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('messages');
+		Schema::drop('category_tickets');
 	}
 
 }
