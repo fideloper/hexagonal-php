85cc612da0a47f92fb01c26d47dd6e410c0817e5
fideloper <fideloper@gmail.com>
2014-04-08 20:00:41 -0400

    updated migration for tickets table

--- app/database/migrations/2014_04_08_211144_create_tickets_table.php
+++ app/database/migrations/2014_04_08_211144_create_tickets_table.php
@@ -15,13 +15,13 @@ class CreateTicketsTable extends Migration {
         Schema::create('tickets', function(Blueprint $table)
         {
             $table->increments('id');
+            $table->integer('category_id');
+            $table->integer('staff_id');
             $table->string('subject');
             $table->string('name');
             $table->string('email');
             $table->tinyInteger('open');
             $table->timestamps();
-            // Staffer - Relationship
-            // Category - Relationship
         });
     }
 
